6f5edc005810bcfb92d9da85682106edfbfe0c79
Federico_Campanozzi <federico.campanozzi@gmail.com>
2023-10-30 21:32:46 +0100

    changed some routes and install npm package

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,11 @@ use App\Http\Controllers\ProductController;
 |
 */
 
-Route::get('/', function () {
+Route::get('/laravel', function () {
     return view('welcome');
 });
 
+Route::get('/', [ProductController::class, 'index'])->name('product.index');
 Route::get('/product', [ProductController::class, 'index'])->name('product.index');
 Route::get('/product/create', [ProductController::class, 'create'])->name('product.create');
 Route::post('/product', [ProductController::class, 'store'])->name('product.store');
